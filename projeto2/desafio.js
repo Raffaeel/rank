fdd406883e73285b64bfa33a4a2ae0dcdf5b0221
Raffaeel <142857378+Raffaeel@users.noreply.github.com>
2024-02-15 09:43:09 -0300

    desafia2

---
+++ projeto2/desafio.js
@@ -0,0 +1,12 @@
+class meuHeroi{
+    constructor(nomeDoheroi,idade,tipo,ataque) {
+            this. nomeDoheroi = "morgan"
+            this. idade= 18
+            this .tipo= "mago"
+            this. ataque ="magia"
+        }
+
+    }
+
+let heroi = new meuHeroi ("morgan",18,"mago","magia")
+        console .log(`o ${heroi.tipo} atacou usando ${heroi.ataque}`);
